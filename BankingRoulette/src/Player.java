49b416571f23c14bbc1d713c7a49cc88caa0bba5
StevenPDang <stevenphi.dang@gmail.com>
2025-11-20 01:46:39 -0600

    First commit

---
+++ BankingRoulette/src/Player.java
@@ -0,0 +1,7 @@
+public class Player {
+    private String name; // (4 bytes ABC\n)
+    private int level; // (stages 1-4)
+    private double wealth; // (up to 10000)
+
+    // getters setters constructors
+}
